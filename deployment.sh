201be7a82b3a76b4c3c2f21d2847d4b55fb54547
jaga9133 <85977422+jaga9133@users.noreply.github.com>
2021-09-01 10:41:28 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "we are doing deployment"
-wget --user=jaga --password=jaga http://3.14.252.150:8081/nexus/service/local/repositories/jaga/content/hello/hello-world/1.0/hello-world-1.0.war
+wget --user=jaga --password=$1 http://3.14.252.150:8081/nexus/service/local/repositories/jaga/content/hello/hello-world/$2/hello-world-$2war
